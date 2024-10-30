6392efa1c0b0bb363f58aeeacfb78345675ba0da
jorgelfer <jorgeferram17@gmail.com>
2024-10-30 09:10:44 -0400

    posts looking good

--- src/UI/SideBar/Post.js
+++ src/UI/SideBar/Post.js
@@ -1,19 +1,19 @@
 import classes from './Post.module.css';
 
 // fill is a utility function that will fill width and height user provided images
-export default function MealItem({ model, price, year, ...props }) {
+export default function Post({ model, price, ...props }) {
   return (
     <article className={classes.post}>
       <header>
         <div className={classes.headerText}>
           <h2>{model}</h2>
           <p>price {price}</p>
-          <p>year {year}</p>
+          <p>year {props["make year"]}</p>
         </div>
       </header>
       <div className={classes.content}>
         <div className={classes.actions}>
-            <button>details</button>
+            <button className={classes["details-button"]}>details</button>
         </div>
       </div>
     </article>
--- src/UI/SideBar/PostSidebar.js
+++ src/UI/SideBar/PostSidebar.js
@@ -2,8 +2,8 @@ import classes from "./PostSidebar.module.css"
 import Post from "./Post"; 
 export default function PostSidebar({posts}) {
   return (
-    <aside className="sideBar">
-       <h2 className="sideBar-text"> Posts</h2> 
+    <aside className={classes.sideBar}>
+       <h2 className={classes["sideBar-text"]}> Posts</h2> 
        <ul className={classes.posts}>
         {posts.map(post => <li key={post.postID}>
           <Post {...post} />
